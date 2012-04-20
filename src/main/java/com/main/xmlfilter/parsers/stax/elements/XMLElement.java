a563dc35a357ca14d55881b5ca78b66f84ac3958
sergiu.indrie <sergiu.indrie@e3c8527d-86d6-4c89-96c3-003f0b15aef6>
2012-04-20 13:38:23 +0000

    Forgot this item - Isolated StAX elements needed for web service business logic. Added a filter&insert command implementation based on a parser independent interface and added a get page command.
    git-svn-id: https://svn.iquestint.com/repos/JavaArea/Projects/xml-filter/trunk@5151 e3c8527d-86d6-4c89-96c3-003f0b15aef6

--- src/main/java/com/main/xmlfilter/parsers/stax/elements/XMLElement.java
+++ src/main/java/com/main/xmlfilter/parsers/stax/elements/XMLElement.java
@@ -5,22 +5,54 @@ import java.util.Map;
 /**
  * Universal XML element
  *
- * @author sergiu.indrie
+ * @author sergiu.indrie // todo move to upper level
  */
-public class XMLElement {
+public class XmlElement {
 
     private ElementType type;
     private String prefix;
     private String localName;
     private String data;
+    private String encoding;
+    private String version;
     private Map<String, String> attributes;
 
-    public XMLElement(ElementType type, String prefix, String localName, String data, Map<String, String> attributes) {
+    public XmlElement(ElementType type, String encoding, String version) {
+        this.type = type;
+        this.encoding = encoding;
+        this.version = version;
+    }
+
+    public XmlElement(ElementType type, String prefix, String localName, String data, Map<String, String> attributes) {
         this.type = type;
         this.prefix = prefix;
         this.localName = localName;
         this.data = data;
         this.attributes = attributes;
+    }
+
+    public static XmlElement createStartDocumentElement(ElementType type, String encoding, String version) {
+        return new XmlElement(type, encoding, version);
+    }
+
+    public static XmlElement createStartElement(Map<String, String> attributes, String prefix, String localName) {
+        return new XmlElement(ElementType.START_ELEMENT, prefix, localName, null, attributes);
+    }
+
+    public static XmlElement createEndElement(String prefix, String localName) {
+        return new XmlElement(ElementType.END_ELEMENT, prefix, localName, null, null);
+    }
+
+    public static XmlElement createDataElement(String data) {
+        return new XmlElement(ElementType.DATA, null, null, data, null);
+    }
+
+     public static XmlElement createCommentElement(String commentText) {
+        return new XmlElement(ElementType.COMMENT, null, null, commentText, null);
+    }
+
+    public static XmlElement createEndDocumentElement() {
+        return new XmlElement(ElementType.END_DOCUMENT, null, null, null, null);
     }
 
     public ElementType getType() {
@@ -63,8 +95,25 @@ public class XMLElement {
         this.attributes = attributes;
     }
 
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
     @Override
     public String toString() {
-        return "XMLElement{" + "type=" + type + ", prefix='" + prefix + '\'' + ", localName='" + localName + '\'' + ", data='" + data + '\'' + ", attributes=" + attributes + '}';
+        return "XmlElement{" + "type=" + type + ", prefix='" + prefix + '\'' + ", localName='" + localName + '\'' + ", data='" + data + '\'' + ", encoding='" + encoding + '\'' + ", version='"
+               + version + '\'' + ", attributes=" + attributes + '}';
     }
 }
